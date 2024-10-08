094aa6229ceaea9275dd532ec095ba104c77963a
SYED175 <syedismailgp@gmail.com>
2024-10-08 10:23:18 +0530

    Use useForm hook with ZodResolver

---
+++ src/components/AmbUloomForm.tsx
@@ -0,0 +1,45 @@
+import { SubmitHandler, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { AmbUloomFormschema, StudentForm } from "../constants/schema";
+import AdmissionForm from "../components/AdmissionForm";
+import apiClient from "../services/api-client";
+import { useNavigate } from "react-router-dom";
+const AmbUloomForm = () => {
+  const navigate = useNavigate();
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors, isSubmitting, isValid },
+    control,
+  } = useForm<StudentForm>({
+    resolver: zodResolver(AmbUloomFormschema),
+  });
+  const onSubmitCallBack: SubmitHandler<StudentForm> = (data) => {
+    const { studentPhoto, ...dataWithoutPhoto } = data;
+    console.log(dataWithoutPhoto);
+    reset();
+    //It works!!
+    apiClient
+      .post("/student/new", dataWithoutPhoto, {
+        withCredentials: true,
+      })
+      .then(() => {
+        reset();
+        !isSubmitting && navigate("/onSubmit");
+      })
+      .catch((error) => console.log(error.response.data.message));
+  };
+  return (
+    <AdmissionForm
+      register={register}
+      handleSubmit={handleSubmit}
+      errors={errors}
+      control={control}
+      onSubmitCallBack={onSubmitCallBack}
+      isSubmitting={isSubmitting}
+    />
+  );
+};
+
+export default AmbUloomForm;
